3cb67af133fa12a66dd8220de90ad511182b6539
unknown <milosmirkovic484484>
2022-12-07 20:14:51 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const menuHamburger = document.querySelector(".menu-hambureger")
+  const menuZat = document.querySelector(".menu-zatvara")
+  const navLinks = document.querySelector(".nav-links")
+  
+  menuHamburger.addEventListener('click',()=>{
+  navLinks.classList.toggle('mobile-menu');
+  
+  
+  });
+
